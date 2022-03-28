1313636a02db9086d7c5c472c0cfc2f4d0019b8d
Michelle Pruitt <86678965+pruittme1@users.noreply.github.com>
2022-03-28 19:26:51 -0400

    adding javascript & adjusting elements

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElmement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElmement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "87b178ac3ff18ae30998e0fe85f9d0ad";
